02ae886e534f0ddb83a7bdbf1847b43c49f3b08d
ArtOxid <bashkin.artem@gmail.com>
2022-11-25 11:52:22 +0600

    first commit

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,74 @@
+package ru.netology;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+            return;
+        }
+        if (newCurrentStation > 9) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void setToEndStation() {
+        currentStation = 9;
+    }
+
+    public int nextStation() {
+        currentStation = currentStation + 1;
+        if (currentStation > 9) {
+            currentStation = 0;
+        }
+        return currentStation;
+    }
+
+    public int previousStation() {
+        currentStation = currentStation - 1;
+        if (currentStation < 0) {
+            currentStation = 9;
+        }
+        return currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void setToMaxVolume() {
+        currentVolume = 10;
+    }
+
+    public int volumeUp() {
+        currentVolume = currentVolume + 1;
+        if (currentVolume >= 10) {
+            currentVolume = 10;
+        }
+        return currentVolume;
+    }
+
+    public int volumeDown() {
+        currentVolume = currentVolume - 1;
+        if (currentVolume <= 0) {
+            currentVolume = 0;
+        }
+        return currentVolume;
+    }
+}
